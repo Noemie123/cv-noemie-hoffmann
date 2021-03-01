d9201040e1dce71bac24a74ac46466e52f6c6be0
Nuwanda <58420859+Noemie123@users.noreply.github.com>
2021-03-01 15:43:27 +0100

    Styling menu with scroll to top

--- libs/js/scroll.js
+++ libs/js/scroll.js
@@ -1,27 +1,49 @@
-(function() {
-    'use strict';
+/* SCROLL TO TOP */
 
-    function trackScroll() {
-        let scrolled = window.pageYOffset;
-        let coords = 100; // Coordonnées en pixel de l'affichage de la flêche
+//Get the button:
+mybutton = document.getElementById("goToTopBtn");
 
-        if (scrolled > coords) {
-            goTopBtn.classList.add('back_to_top-show'); // Montre la flêche
-        }
-        if (scrolled < coords) {
-            goTopBtn.classList.remove('back_to_top-show'); // fait disparaître la flêche
-        }
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+/* SMOOTH SCROLL FOR NAVBAR */
+
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
     }
-
-    function backToTop() {
-        if (window.pageYOffset > 0) {
-            window.scrollBy(0, -80); // Vitesse du scroll
-            setTimeout(backToTop, 10);
-        }
-    }
-
-    let goTopBtn = document.querySelector('.back_to_top');
-
-    window.addEventListener('scroll', trackScroll);
-    goTopBtn.addEventListener('click', backToTop);
-})();
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
